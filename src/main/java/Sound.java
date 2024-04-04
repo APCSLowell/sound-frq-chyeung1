74f1d487b630581edb31a1010683eb082352056e
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-04-04 12:07:47 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -16,7 +16,16 @@ public class Sound
   public int limitAmplitude(int limit)
   {  
     /* to be implemented in part (a) */
-    return 0;
+    int numChanges = 0;
+    for(int i = 0; i < samples.length; i++){
+      if(samples[i] > limit){
+        samples[i] = limit;
+        numChanges++;
+    } else if (samples[i] < -1 * limit) {
+        samples[i] = -1 * limit;
+        numChanges++;
+      }
+    return numChanges;
   }
 
 
@@ -29,5 +38,6 @@ public class Sound
   public void trimSilenceFromBeginning()
   {
     /* to be implemented in part (b) */
+    
   }
 }
